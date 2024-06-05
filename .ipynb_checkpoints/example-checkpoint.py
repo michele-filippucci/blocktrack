b29e6eda2e552f492fe59882a304b94cd4b6b7fb
michele-filippucci <michele.filippucci@yahoo.it>
2024-06-05 09:38:29 +0100

    example.py updated

--- .ipynb_checkpoints/example-checkpoint.py
+++ .ipynb_checkpoints/example-checkpoint.py
@@ -18,10 +18,15 @@ fn_out = "/home/michele/DATA/ERA5/ERA5_northem_2.5x2.5_DAV_daily_1959-2021.nc" #
 print("Importing data from: " + fn)
 ds = xr.load_dataset(fn)
 print("Dataset correctly loaded")
-ds = bt.DAV(ds,mer_gradient_filter = True,data_return=True)
+ds = ds = bt.DAV(ds,mer_gradient_filter = True,data_return=True)
 print("DAV function correctly executed")
-#ds = bt.ContourTracking2D(ds,pers=4,min_area = 500000,max_length=28,data_return=True)
-#print("ContourTracking2D function correclty executed")
+ds,dic = bt.ContourTracking2D(ds,var_name='DAV',data_return=True,char_dic=True,exp_dic=False)
+#a dictionary object is created which contains the features of the tracked events.
+print("ContourTracking2D function correclty executed")
+ds,dic_filt = bt.FilterEvents(ds,dic,data_return=True,var_name='GHA',fn_dic_out=string_dic_out,
+              pers_min = 5,pers_max = 25,min_area = 2e6,max_area=10e6,max_ar=100,max_avg_dist=10000)
+#the filtering is performed based on the features stored in the dictionary. The new set of events is then
+#stored is a new dictionary (dic_filt)
 ds.to_netcdf(fn_out)
 print("Data created and exported in: " + fn_out)
   
